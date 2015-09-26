b17f4604d98f35d9bf57eff951492f68db462304
Felipe VieiraCortes <felipevieiracortes@gmail.com>
2015-09-26 18:03:06 -0300

    gauss

--- LAB5/gauss.h
+++ LAB5/gauss.h
@@ -1,2 +1,1 @@
-double modulo(double a);
 double** fatoracao( int n, double** A);
---
+++ LAB5/matriz.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+
+// Cria Matriz MxN
+double** mat_cria(int m, int n)
+{
+	double** A;
+	int i;
+	A = (double**)malloc(m*sizeof(double*));
+	for(i=0; i < m; i++)
+	{
+		A[i] = (double*)malloc(n*sizeof(double));
+	}
+	return A;
+}
+
+// Libera o espaço alocado pela matriz
+void mat_libera(int m, double** A)
+{
+	int i;
+	for(i=0; i < m; i++)
+	{
+		free(A[i]);
+	}
+	free(A);
+}
+// Transposta da matriz
+void mat_transposta(int m, int n, double** A, double** T)
+{
+	int i, j;
+	for(i=0; i < m; i++)
+	{
+		for(j=0;j < n; j++)
+		{
+			T[j][i] = A[i][j];
+		}
+	}
+}
+// Multiplicação de Vetor x Matriz
+void mat_multv(int m, int n, double** A, double* v, double* w)
+{
+/* matriz MxN . vetor Nx1 -> vetor Mx1*/
+	int i, j;
+	for(i = 0; i < m; i++)
+		w[i] = 0;
+	for(i = 0; i < m; i++)
+	{
+		for(j = 0; j < n; j++)
+		{
+			w[i] += A[i][j] * v[j];
+		}
+	}
+}
+// Multiplicação de Matrizes
+void mat_multm(int m, int n, int q, double** A, double** B, double** C)
+{
+	int i, j, k, soma = 0;
+	for(i = 0; i < m; i ++)
+	{
+		for(j = 0; j < q; j++)
+		{
+			C[i][j] = 0;
+		}
+	}
+	for(i = 0; i < m; i++)
+	{
+		for(j = 0; j < q; j++)
+		{
+			for(k = 0; k < n; k++)
+			{
+				soma+= A[i][k] * B[k][j];
+			}
+			C[i][j] = soma;
+			soma = 0;
+		}
+	}
+}
+
